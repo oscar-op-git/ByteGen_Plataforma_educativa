b806ec83e31d870e90c57139ac1a600a151fe744
oscarin <oskarblot123@gmail.com>
2025-10-02 23:01:09 -0400

    Agregando estilo al login inicial

--- frontend/pages/LoginPages.tsx
+++ frontend/pages/LoginPages.tsx
@@ -1,10 +1,13 @@
+import "../styles/login.css";
 import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="login-container">
-      <h1>Iniciar Sesión</h1>
-      <LoginForm />
+    <div className="login-page">
+      <div className="login-box">
+        <h1 className="title">Iniciar Sesión</h1>
+        <LoginForm />
+      </div>
     </div>
   );
 }
