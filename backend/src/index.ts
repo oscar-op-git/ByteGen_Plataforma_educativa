97d7aa9c9d88e64f7dffa67e56316a886318a09e
oscarin <oskarblot123@gmail.com>
2025-10-06 19:37:24 -0400

    feat: Verificar y confirmar conexión inicial a la DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
-import express from 'express'
-
+import express from 'express';
 const app = express();
+
+import indexRoutes from './routes/index.js';
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.use(indexRoutes);
 
 app.listen(4000);
 console.log('server on port',4000)
