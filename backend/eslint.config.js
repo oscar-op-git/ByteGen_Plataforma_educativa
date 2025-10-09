5a07123a8eaca18b6f5d5852d8e8ef9a760c3899
oscarin <oskarblot123@gmail.com>
2025-10-08 23:41:02 -0400

    chore: agregamos prettier en el backend y frontend

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import globals from 'globals'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
@@ -10,7 +12,11 @@ export default defineConfig([
     extends: [
       js.configs.recommended,        // reglas básicas de JS
       tseslint.configs.recommended,  // reglas de TS
+       prettier, //Desactiva conflictos de formato
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.node, // habilita variables globales de Node.js
@@ -18,6 +24,7 @@ export default defineConfig([
     rules: {
       "@typescript-eslint/no-unused-vars": "warn", // avisa si no usas una variable
       "no-console": "off", // en backend a veces sí usamos console.log
+      "prettier/prettier": "warn", // Advierte si no sigue formato de Prettier
     }
   }
 ])
