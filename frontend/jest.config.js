e25d2f777278ad803ac3fc4723288925f07ebc3b
kevin-rioja <kevinrioja212@gmail.com>
2025-10-11 17:53:45 -0400

    Configuracion completa de jest para frontend

---
+++ frontend/jest.config.js
@@ -0,0 +1,17 @@
+export default {
+    displayName: "frontend",
+    preset: "ts-jest/presets/js-with-ts-esm",
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+    roots: ["<rootDir>/src"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+    transform: {
+      "^.+\\.tsx?$": [
+        "ts-jest",
+        {
+          useESM: true,  
+          tsconfig: "tsconfig.jest.json"
+        }
+      ]
+    }
+  };
