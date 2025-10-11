2156535ebd8b9e244df88da6750e273dab7ccc89
kevin-rioja <kevinrioja212@gmail.com>
2025-10-10 23:50:31 -0400

    Configuracion de jest para backend

---
+++ backend/src/__tests__/ejemplo.test.ts
@@ -0,0 +1,11 @@
+describe("Prueba básica del backend", () => {
+    it("debería sumar correctamente", () => {
+      const resultado = 2 + 3;
+      expect(resultado).toBe(5);
+    });
+  
+    it("debería comparar strings correctamente", () => {
+      const mensaje = "Hola Jest";
+      expect(mensaje).toContain("Jest");
+    });
+  });
