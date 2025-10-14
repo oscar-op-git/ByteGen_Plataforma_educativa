7aceb03444caeaceecdc2b84ce594f0d8b83a12c
oscarin <oskarblot123@gmail.com>
2025-10-13 21:52:35 -0400

    style: Corregir errores de estilo y formato con Prettier

--- backend/src/__tests__/ejemplo.test.ts
+++ backend/src/__tests__/ejemplo.test.ts
@@ -1,10 +1,10 @@
-describe("Prueba básica del backend", () => {
-    it("debería sumar correctamente", () => {
-      const resultado = 2 + 3;
-      expect(resultado).toBe(5);
-    });
-    it("debería comparar strings correctamente", () => {
-      const mensaje = "Hola Jest";
-      expect(mensaje).toContain("Jest");
-    });
-  });
+describe('Prueba básica del backend', () => {
+  it('debería sumar correctamente', () => {
+    const resultado = 2 + 3
+    expect(resultado).toBe(5)
+  })
+  it('debería comparar strings correctamente', () => {
+    const mensaje = 'Hola Jest'
+    expect(mensaje).toContain('Jest')
+  })
+})
--- backend/src/jest.config.js
+++ backend/src/jest.config.js
@@ -1,10 +1,9 @@
-
 export default {
-    displayName: "backend",
-    rootDir: "./",
-    preset: "ts-jest/presets/default-esm", // preset ESM
-        testEnvironment: "node",
-        extensionsToTreatAsEsm: [".ts"],
-        roots: ["<rootDir>/src"],
-        moduleFileExtensions: ["ts", "js", "json", "node"]
-  };
+  displayName: 'backend',
+  rootDir: './',
+  preset: 'ts-jest/presets/default-esm', // preset ESM
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+}
