6905a668b171ef0d4f59951e5eb9af39f2f48664
kevin-rioja <kevinrioja212@gmail.com>
2025-10-16 23:39:04 -0400

    Terminado la primera version de registro de usuario

---
+++ frontend/src/types/auth.types.ts
@@ -0,0 +1,27 @@
+export interface FormData {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface FormErrors {
+  name?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
+
+export interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface PasswordValidation {
+  hasMinLength: boolean;
+  hasUpperCase: boolean;
+  hasLowerCase: boolean;
+  hasNumber: boolean;
+  hasSpecialChar: boolean;
+}
