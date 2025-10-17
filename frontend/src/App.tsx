8783092e70b64efebf2b5adad1668ba9994847c6
kevin-rioja <kevinrioja212@gmail.com>
2025-10-16 23:39:04 -0400

    Terminado la primera version de registro de usuario

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPages from "./pages/LoginPages";
+import {RegisterPage} from "./pages/RegisterPage"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPages />} />
+        <Route path="/Registro" element={<RegisterPage/>}/>
       </Routes>
     </Router>
   )
