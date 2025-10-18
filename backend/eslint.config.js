83ea9de06b68ee9e4cc12aed635c5aea0cfe475c
oscarin <oskarblot123@gmail.com>
2025-10-17 22:40:18 -0400

    chore: configuramos carpetas ignoradas

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -6,38 +6,34 @@ import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-    // 1. OBJETO DEDICADO PARA EXCLUSIONES GLOBALES (¡Va primero!)
-    {
-        ignores: [
-            'dist/**', 
-            'src/generated/**', // Ignora el código de Prisma
-            'node_modules/**', 
-            '.env' 
-        ]
-    },
+  // 1. OBJETO DEDICADO PARA EXCLUSIONES GLOBALES (¡Va primero!)
+  {
+    ignores: [
+      'dist/**',
+      'src/generated/**', // Ignora el código de Prisma
+      'node_modules/**',
+      '.env',
+    ],
+  },
 
-    // 2. TU CONFIGURACIÓN DE REGLAS (Ahora sin la propiedad 'ignores')
-    {
-        files: ['**/*.{ts,js}'], // aplica a .ts y .js
-        
-        // ¡QUITAMOS la propiedad ignores de este objeto!
-        
-        extends: [
-          js.configs.recommended, 
-          tseslint.configs.recommended,
-          prettier,
-        ],
-        plugins: {
-          prettier: prettierPlugin,
-        },
-        languageOptions: {
-          ecmaVersion: 2020,
-          globals: globals.node,
-        },
-        rules: {
-          '@typescript-eslint/no-unused-vars': 'warn',
-          'no-console': 'off',
-          'prettier/prettier': 'warn',
-        },
+  // 2. TU CONFIGURACIÓN DE REGLAS (Ahora sin la propiedad 'ignores')
+  {
+    files: ['**/*.{ts,js}'], // aplica a .ts y .js
+
+    // ¡QUITAMOS la propiedad ignores de este objeto!
+
+    extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
+    plugins: {
+      prettier: prettierPlugin,
     },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'off',
+      'prettier/prettier': 'warn',
+    },
+  },
 ])
