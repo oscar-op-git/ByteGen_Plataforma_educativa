99b2e43fc3efe34af3cb14c5f503099c7e65e320
Jorge Condori <ramjorgemax@gmail.com>
2025-10-17 21:42:08 -0400

    Subiendo prueba para ver el github accions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,3 +29,4 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+//BORRAME PORFAVOR, PRUEBA PARA VER LO DE GITHUB ACCIONS
