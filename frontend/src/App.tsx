e01b2214bbab7211275e7f9fb7900cc2462cffef
oscarin <oskarblot123@gmail.com>
2025-10-20 23:58:05 -0400

    corrigiendo formato y formatos incorrectos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPages from './pages/LoginPages'
 import { RegisterPage } from './pages/RegisterPage'
 
