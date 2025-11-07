79d51dff90f1941606b8d6e4efba516d0eb55f42
kevin-rioja <kevinrioja212@gmail.com>
2025-11-07 00:23:19 -0400

    feature: Correciones para funcionar con tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'react-hot-toast'
+import './styles/output.css'
 
 
 createRoot(document.getElementById('root')!).render(
